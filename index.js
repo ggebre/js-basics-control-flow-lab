50b02c50c7b69001c7d98e545862ae0e9d2dec86
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-12 08:44:38 -0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rides){
   // Write your code here!
+  
+  if (rides <= 400) {
+    return "This one is on me!"
+  } 
+  if (rides > 2500) {
+    return 'No can do.'
+  } 
+  if (rides > 2000 && rides <= 2500){
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC'){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+
+  }else if (tip === 'not as generous'){
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
+
